a2486ddcfdc58df457e5903de3959713fe17dc65
Patrick Auernig <patrick.auernig@mykolab.com>
2018-04-19 22:13:41 +0200

    Move database tests into database module

--- src/core/mech/databases/mongodb.rs
+++ src/core/mech/databases/mongodb.rs
@@ -42,3 +42,64 @@ impl MongoDatabase {
         self.client.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use core::mech::configuration::DatabaseConfiguration;
+    use super::MongoDatabase;
+    use super::MongoClient;
+    use super::ThreadedClient;
+    use mongodb::db::ThreadedDatabase;
+
+    fn get_db_client() -> MongoClient {
+        let handler: String = "mongodb".to_owned();
+        let address: String = "127.0.0.1".to_owned();
+        let port: i16 = 27017;
+        let authenticate: bool = false;
+        let username: String = "root".to_owned();
+        let password: String = "5up3r53cur3pa55w0rd".to_owned();
+        let database: String = "omega".to_owned();
+        let db_config: DatabaseConfiguration = DatabaseConfiguration::new(handler, address, port, username, password, authenticate, database);
+        let db_instance: MongoDatabase = MongoDatabase::new(db_config);
+        let db_client: MongoClient = db_instance.get_client();
+        return db_client;
+    }
+
+    #[test]
+    fn test_database_count() {
+        let db_client: MongoClient = get_db_client();
+        let db_database = db_client.db("test");
+        let db_collection = db_database.collection("test");
+        let count_success: bool = match db_collection.count(None, None) {
+            Ok(_) => true,
+            Err(_) => false
+        };
+        assert!(count_success);
+    }
+
+    #[test]
+    fn test_database_insert() {
+        let db_client: MongoClient = get_db_client();
+        let db_database = db_client.db("test");
+        let db_collection = db_database.collection("test");
+        let document = doc! { "test_string": "Alex loves spaghetti." };
+        let insert_success: bool = match db_collection.insert_one(document, None) {
+            Ok(_) => true,
+            Err(_) => false
+        };
+        assert!(insert_success);
+    }
+
+    #[test]
+    fn test_database_delete() {
+        let db_client: MongoClient = get_db_client();
+        let db_database = db_client.db("test");
+        let db_collection = db_database.collection("test");
+        let document = doc! { "test_string": "Alex loves spaghetti." };
+        let delete_success: bool = match db_collection.delete_one(document, None) {
+            Ok(_) => true,
+            Err(_) => false
+        };
+        assert!(delete_success);
+    }
+}
